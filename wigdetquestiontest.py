af0514c8d5a566dec1278208631a97fe6f7a92da
Modicolitor <Bjoern.Eckhardt@gmx.de>
2020-07-01 15:58:33 +0200

    widgettester gemacht und dann lösung gefunden follows

--- wigdetquestiontest.py
+++ wigdetquestiontest.py
@@ -14,6 +14,94 @@ from bpy.types import (
 )
 from mathutils import Vector
 
+
+
+class BE_GizmoGroup(GizmoGroup):
+    bl_idname = "OBJECT_GGT_PLANARCONNECTOR"
+    bl_label = "Positioning GizmoGroup"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'WINDOW'
+    bl_options = {'3D', 'PERSISTENT'}
+
+    @classmethod
+    def poll(cls, context):
+        ob = context.object
+        return ob != None
+          
+
+    def setup(self, context):
+        ob = context.object
+
+        #'''
+        ####Center Version start 
+        centerR = self.gizmos.new(CenterShapeWidget.bl_idname)
+        props = centerR.target_set_operator("object.ot_scalemini")
+        centerR.matrix_basis = ob.matrix_world.normalized()
+        centerR.use_draw_offset_scale = True 
+        centerR.matrix_offset[0][3] = -2
+        centerR.matrix_offset[1][3] = 2
+        centerR.matrix_offset[2][3] = 2
+        
+        #centerR.matrix_basis[0][3] -= 2  ## moving the centered Gizmoshape 
+        #centerR.matrix_basis[1][3] += 2
+        #centerR.matrix_basis[2][3] += 2
+        #centerR.color = 0.05, 0.2, 0.8
+        self.WidCR = centerR 
+
+        centerL = self.gizmos.new(CenterShapeWidget.bl_idname)
+        props = centerL.target_set_operator("object.ot_scalemini")
+        
+        centerL.use_draw_offset_scale = True 
+        centerL.matrix_basis = ob.matrix_world.normalized()
+        centerL.matrix_offset[0][3] = -2  ## moving the centered Gizmoshape 
+        centerL.matrix_offset[1][3] = -2
+        centerL.matrix_offset[2][3] = 2
+        centerL.color = 0.05, 0.2, 0.8
+        self.WidCL = centerL 
+
+        '''
+        ####cornered start 
+        cornerR = self.gizmos.new(CornerRShapeWidget.bl_idname)
+        props = cornerR.target_set_operator("object.ot_scalemini")
+        cornerR.matrix_basis = ob.matrix_world.normalized()
+        cornerR.color = 0.05, 0.2, 0.8
+        self.CornR = cornerR 
+
+        cornerL = self.gizmos.new(CornerLShapeWidget.bl_idname)
+        props = cornerL.target_set_operator("object.ot_scalemini")
+        cornerL.matrix_basis = ob.matrix_world.normalized()
+        cornerL.color = 0.05, 0.2, 0.8
+        self.CornL = cornerL 
+        ###cornered end 
+        '''
+
+    def refresh(self, context):
+        ob = context.object
+        #'''
+        ###centered start
+        centerR = self.WidCR 
+        centerR.matrix_basis = ob.matrix_world.normalized()
+        centerR.matrix_offset[0][3] = -2
+        centerR.matrix_offset[1][3] = 2
+        centerR.matrix_offset[2][3] = 2
+
+        centerL = self.WidCL 
+        centerL.matrix_basis = ob.matrix_world.normalized()
+        centerL.matrix_offset[0][3] = -2
+        centerL.matrix_offset[1][3] = -2
+        centerL.matrix_offset[2][3] = 2
+        ###centered end 
+        '''
+        #####cornered start 
+        cornerL = self.CornL
+        cornerL.matrix_basis = ob.matrix_world.normalized()
+        cornerR = self.CornR
+        cornerR.matrix_basis = ob.matrix_world.normalized()
+        #####cornered end
+        #'''  
+
+
+
 #####centered 
 centered0 = Vector((0.0, 1.0, -1.0))
 centered1 = Vector((0.866, -0.50, -1.0))
@@ -30,7 +118,7 @@ center_shape = (
 #####corner R 
 cornerR0 = Vector((-2.0, 3.0, 1.0))
 cornerR1 = Vector((-1.13, 1.5, 1.0))
-cornerR2 = Vector((-2.866, 1.50, 1.0))
+cornerR2 = Vector((-2.87, 1.50, 1.0))
 cornerR3 = Vector((-2.0, 2.0, 3.0))
 cornerR_shape = (
     cornerR0,cornerR1,cornerR2,
@@ -40,40 +128,21 @@ cornerR_shape = (
 )
 
 ######corner L
-cornerL0 = Vector((-2.0, 3.0, 1.0))
-cornerL1 = Vector((-1.134, 1.5, 1.0))
-cornerL2 = Vector((-2.866, 1.50, 1.0))
-cornerL3 = Vector((-2.0, 2.0, 3.0))
+cornerL0 = Vector((-2.0, -1.0, 1.0))
+cornerL1 = Vector((-1.13, -2.5, 1.0))
+cornerL2 = Vector((-2.87, -2.5, 1.0))
+cornerL3 = Vector((-2.0, -2.0, 3.0))
 
-cornerR_shape = (
+cornerL_shape = (
     cornerL0,cornerL1,cornerL2,
     cornerL0,cornerL1,cornerL3,
     cornerL2,cornerL1,cornerL3,
     cornerL0,cornerL2,cornerL3,
 )
 
-
-
 class CenterShapeWidget(Gizmo):
-    bl_idname = "VIEW3D_GT_Centershape"
-    bl_target_properties = (
-        {"id": "scale", "type": 'FLOAT', "array_length": 1},
-    )
-
-    __slots__ = (
-        "custom_shape",
-        "init_mouse_y",
-        "init_value",
-    )
-
+    bl_idname = "VIEW3D_GT_CENTERSHAPE"
     
-
-    def _update_offset_matrix(self):
-        # offset behind the light
-        # print("jaja")
-        #self.matrix_offset.col[3][2] = context.object.matrix_world
-        pass
-
     def draw(self, context):
         # self._update_offset_matrix()
         self.draw_custom_shape(self.custom_shape)
@@ -86,71 +155,82 @@ class CenterShapeWidget(Gizmo):
         if not hasattr(self, "custom_shape"):
             self.custom_shape = self.new_custom_shape(
                 'TRIS', center_shape)
+    
+class CornerLShapeWidget(Gizmo):
+    bl_idname = "VIEW3D_GT_CORNERL"
+    
+    def draw(self, context):
+        # self._update_offset_matrix()
+        self.draw_custom_shape(self.custom_shape)
 
-    def invoke(self, context, event):
-        self.init_mouse_y = event.mouse_y
-        # self.init_value = self.target_get_value("offset")
-        return {'RUNNING_MODAL'}
+    def draw_select(self, context, select_id):
+        # self._update_offset_matrix()
+        self.draw_custom_shape(self.custom_shape, select_id=select_id)
 
-    def exit(self, context, cancel):
-        context.area.header_text_set(None)
-        # if cancel:
-        #    self.target_set_value("offset", self.init_value)
+    def setup(self):
+        if not hasattr(self, "custom_shape"):
+            self.custom_shape = self.new_custom_shape(
+                'TRIS', cornerL_shape)
 
-    def modal(self, context, event, tweak):
-        delta = (event.mouse_y - self.init_mouse_y) / 10.0
-        # if 'SNAP' in tweak:
-        #    delta = round(delta)
-        # if 'PRECISE' in tweak:
-        #    delta /= 10.0
-        # value = self.init_value - delta
-        # self.target_set_value("offset", value)
-        # context.area.header_text_set("My Gizmo: %.4f" % value)
-        return {'RUNNING_MODAL'}
+class CornerRShapeWidget(Gizmo):
+    bl_idname = "VIEW3D_GT_CORNERR"
+    
+    def draw(self, context):
+        # self._update_offset_matrix()
+        self.draw_custom_shape(self.custom_shape)
 
+    def draw_select(self, context, select_id):
+        # self._update_offset_matrix()
+        self.draw_custom_shape(self.custom_shape, select_id=select_id)
 
-class PUrP_PlanarGizmo(GizmoGroup):
-    bl_idname = "OBJECT_GGT_PLANARCONNECTOR"
-    bl_label = "Positioning GizmoGroup"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'WINDOW'
-    bl_options = {'3D', 'PERSISTENT'}
+    def setup(self):
+        if not hasattr(self, "custom_shape"):
+            self.custom_shape = self.new_custom_shape(
+                'TRIS', cornerR_shape)   
+
+class BE_OT_scale(bpy.types.Operator):
+    bl_idname = "object.ot_scalemini"
+    bl_label = "BE_PP_scalemini"
 
     @classmethod
     def poll(cls, context):
         ob = context.object
-        return ob != None
-          
+        return ob !=None
 
-    def setup(self, context):
+    def execute(self, context):
         ob = context.object
+        ob.scale.x = self.value
+        return {'FINISHED'}
 
-        print("setup Gizmo")
-        ####Center Version start 
-        centerR = self.gizmos.new(CenterShapeWidget.bl_idname)
-        props = centerR.target_set_prop("offset", ob.scale, "x")
-        centerR.matrix_basis = ob.matrix_world.normalized()
-        centerR.matrix_basis[0][3] += 2
+    def modal(self, context, event):
+        if event.type == 'MOUSEMOVE':  # Apply
+            self.delta = event.mouse_x - self.init_value
+            self.value = self.init_position + self.delta / 1000
+            self.execute(context)
+        elif event.type == 'LEFTMOUSE':  # Confirm
+            return {'FINISHED'}
+        elif event.type in {'RIGHTMOUSE', 'ESC'}:  # Cancels
+            ob.scale.x = self.init_value 
+            return {'CANCELLED'}
+        return {'RUNNING_MODAL'}
 
-        #centerR.line_width = 3
-        centerR.color = 0.05, 0.2, 0.8
-        #centerR.alpha = 0.5
-        #centerR.color_highlight = 0.03, 0.05, 1.0
-        #centerR.alpha_highlight = 1.0
-        self.WidCR = centerR 
+    def invoke(self, context, event):
+        ob = context.object
+        self.init_position = ob.scale.x
+        self.init_value = event.mouse_x
+        self.value = ob.scale.x
+        self.execute(context)
 
-    def refresh(self):
-        ###center start
-        centerR = self.WidCR 
-        centerR.matrix_basis = ob.matrix_world.normalized()
-        centerR.matrix_basis[0][3] += 2
-        
-        
-        
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+
 
 classes = (
-    PUrP_PlanarGizmo,
+    BE_GizmoGroup,
     CenterShapeWidget,
+    CornerLShapeWidget,
+    CornerRShapeWidget,
+    BE_OT_scale,
 )
 
 register, unregister = bpy.utils.register_classes_factory(classes)
